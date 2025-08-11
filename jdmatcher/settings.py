b3a908a2e40c2945938fd5f3d267c1be9fd609f2
srinivaskanchi-dev <srinivaskanchi752@gmail.com>
2025-08-11 21:57:00 +0530

    Load secret key from .env instead of hardcoding

--- jdmatcher/settings.py
+++ jdmatcher/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$w=qwkrbq6m4a)(8oz%50sx!u%ukyu^zq%8k_v+n5rzwx16+zs'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
